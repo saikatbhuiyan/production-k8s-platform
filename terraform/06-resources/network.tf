3a42ae4815abe887df917513c3425a522523bae8
saikatbhuiyan <shaikotbhuiya@gmail.com>
2026-06-02 08:47:54 +0600

    VPC and ig and subnet setup done

--- terraform/06-resources/network.tf
+++ terraform/06-resources/network.tf
@@ -1,20 +1,62 @@
+locals {
+  common_tags = {
+    ManagedBy  = "Terraform"
+    Project    = "resources"
+    CostCenter = "1234"
+  }
+}
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
-  tags = {
-    Name      = "resources"
-    ManagedBy = "Terraform"
-    Project   = "resources"
-  }
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "main-vpc"
+    }
+  )
 }
 
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
 
-  tags = {
-    Name      = "public-subnet"
-    ManagedBy = "Terraform"
-    Project   = "resources"
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "public-subnet"
+    }
+  )
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "main-internet-gateway"
+    }
+  )
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "public-route-table"
+    }
+  )
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
   }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
 }
